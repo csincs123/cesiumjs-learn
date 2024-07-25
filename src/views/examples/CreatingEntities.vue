0ecdceeb6a1854eaf71b9d2e898f23898237350a
chenshuai <chenshuai@mail.bdsmc.net>
2024-07-25 08:46:37 +0800

    提取选择下拉框组件

---
+++ src/views/examples/CreatingEntities.vue
@@ -0,0 +1,50 @@
+<script setup>
+import * as Cesium from 'cesium'
+import {ref, onMounted} from 'vue'
+import selectEdit from '@/components/selectEdit/index.vue'
+
+let viewer = null
+onMounted(async () => {
+  viewer = new Cesium.Viewer('cesiumContainer')
+})
+const options = [
+    {label: "第一个实体", value: firstEntity}
+]
+function firstEntity () {
+  const wyoming = viewer.entities.add({
+    polygon: {
+      hierarchy: Cesium.Cartesian3.fromDegreesArray([
+        -109.080842, 45.002073, -105.91517, 45.002073, -104.058488, 44.996596,
+        -104.053011, 43.002989, -104.053011, 41.003906, -105.728954, 40.998429,
+        -107.919731, 41.003906, -109.04798, 40.998429, -111.047063, 40.998429,
+        -111.047063, 42.000709, -111.047063, 44.476286, -111.05254, 45.002073,
+      ]),
+      height: 0,
+      material: Cesium.Color.RED.withAlpha(0.5),
+      outline: true,
+      outlineColor: Cesium.Color.BLACK,
+    },
+  });
+  wyoming.polygon.height = 200000;
+  wyoming.polygon.extrudedHeight = 250000;
+
+  viewer.zoomTo(wyoming);
+}
+
+function shapesVolumes () {
+
+}
+</script>
+
+<template>
+  <div id = "cesiumContainer" class = "map">
+    <selectEdit :options = "options" ></selectEdit>
+  </div>
+</template>
+
+<style>
+  .map{
+    height: 100%;
+    width:100%
+  }
+</style>
--- src/views/examples/StylingFiltering3d.vue
+++ src/views/examples/StylingFiltering3d.vue
@@ -1,6 +1,8 @@
 <script setup>
 import * as Cesium from 'cesium'
-import { onMounted } from 'vue'
+import {ref, onMounted } from 'vue'
+
+const curOptions = ref('')
 
 let viewer = null
 let buildingsTileset = null
@@ -8,7 +10,25 @@ const options = [
     {label: "应用基础样式", value: 'Apply a basic style'},
     {label: "基于属性显示要素", value: 'Show features based on a property'},
     {label: "颜色特征使用条件", value: 'Color features using conditions'},
+    {label: "处理未定义属性", value: 'Handle undefined properties'},
+    {label: "点云样式", value: 'Point cloud styling example'},
 ]
+
+const HandleUndefinedProperties = function () {
+    buildingsTileset.style = new Cesium.Cesium3DTileStyle({
+        color: {
+        conditions: [
+            ["${material} === null", "color('white')"],
+            ["${material} === 'glass'", "color('skyblue', 0.5)"],
+            ["${material} === 'concrete'", "color('grey')"],
+            ["${material} === 'brick'", "color('indianred')"],
+            ["true", "color('white')"],
+        ],
+        },
+    });
+}
+
+
 const basicStyle = function () {
     buildingsTileset.style = new Cesium.Cesium3DTileStyle({
         color: {
@@ -42,15 +62,75 @@ const colorFeatures = function () {
         },
     });
 }
+
+const pointCloudStyle = async function () {
+    console.log('Cesium.Matrix4.IDENTITY', Cesium.Matrix4.IDENTITY)
+    // 取消lookAt效果，恢复到默认相机位置
+    viewer.scene.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+    let pointCloudTileset;
+    try {
+        // A ~10 billion point 3D Tileset of the city of Montreal, Canada captured in 2015 with a resolution of 20 cm. Tiled and hosted by Cesium ion.
+        pointCloudTileset = await Cesium.Cesium3DTileset.fromIonAssetId(28945, {
+            pointCloudShading: {
+                attenuation: true,
+                maximumAttenuation: 2,
+            },
+        });
+        viewer.scene.primitives.add(pointCloudTileset);
+    } catch (error) {
+        console.log(`Error loading tileset: ${error}`);
+    }
+    viewer.camera.setView({
+        destination: new Cesium.Cartesian3(
+            1269319.8408991008,
+            -4293301.826913256,
+            4527724.561372451
+        ),
+        orientation: {
+            direction: new Cesium.Cartesian3(
+            -0.742505030107832,
+            -0.3413204607149223,
+            -0.5763563336703441
+            ),
+            up: new Cesium.Cartesian3(
+            -0.04655102331027917,
+            -0.8320643756800384,
+            0.5527222421370013
+            ),
+        },
+        easingFunction: Cesium.EasingFunction.QUADRATIC_IN_OUT,
+    });
+    pointCloudTileset.style = new Cesium.Cesium3DTileStyle({
+        color: {
+            conditions: [
+            ["${Classification} === 2", "color('brown')"],       // ground
+            ["${Classification} === 3", "color('greenyellow')"], // low vegetation
+            ["${Classification} === 4", "color('green')"],       // medium vegetation
+            ["${Classification} === 5", "color('darkgreen')"],   // high vegetation
+            ["true", "color('white')"]
+            ]
+        }
+    });
+}
+
+
 const handleSelect = (value) => {
   switch (value) {
     case 'Apply a basic style':
         basicStyle()
-      break;
+        break;
     case 'Show features based on a property':
         showFeatures()
+        break
     case 'Color features using conditions':
         colorFeatures()
+        break
+    case 'Point cloud styling example':
+        pointCloudStyle()
+        break
+    case 'Handle undefined properties':
+        HandleUndefinedProperties()
+        break
     default:
       break;
   }
@@ -67,14 +147,14 @@ onMounted(async () => {
         new Cesium.Cartesian3.fromDegrees(144.96007, -37.82249),
         new Cesium.Cartesian3(0.0, -1500.0, 1200.0)
     );
-    })
+})
 </script>
 
 <template>
     <div id="cesiumContainer" class="map">
         <div class="select">
             <el-select
-                v-model="value"
+                v-model="curOptions"
                 @change="handleSelect"
             >
                 <el-option
