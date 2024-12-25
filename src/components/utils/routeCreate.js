af1b74becc14a631fa89303af7493da8eb0bb736
csincs123 <1055321240@qq.com>
2024-12-25 21:36:24 +0800

    deploy only dist file

--- src/components/utils/routeCreate.js
+++
@@ -1,8 +0,0 @@
-export async function createFileRoute () {
-    const vueFile = import.meta.glob("../../views/examples/*.vue")
-    console.log('vueFile', vueFile)
-}
-
-export function createRouteJson () {
-
-}
