449b6997961b744a5f218d682ea4c19e71a5eed0
csincs123 <1055321240@qq.com>
2024-12-27 11:39:16 +0800

    显示鼠标位置：四种坐标的区别

--- src/views/examples/interactive/mousePosition.vue
+++ src/views/examples/interactive/mousePosition.vue
@@ -7,9 +7,8 @@ const form = reactive({
   cartesian3: '',
   pickPosition: '',
   rayPosition: '',
-  type: [],
-  resource: '',
-  desc: '',
+  modelCartographic: '',
+  rayCartographic: ''
 })
 
 let viewer = null
@@ -17,6 +16,14 @@ onMounted(async() => {
     viewer = new Cesium.Viewer('cesiumContainer', {
         terrain: Cesium.Terrain.fromWorldTerrain()
     })
+    let buildingsTileset = await Cesium.createOsmBuildingsAsync()
+    viewer.scene.primitives.add(buildingsTileset)
+
+    // Adjust the camera to look at Melbourne
+    viewer.camera.lookAt(
+        new Cesium.Cartesian3.fromDegrees(144.96007, -37.82249),
+        new Cesium.Cartesian3(0.0, -1500.0, 1200.0)
+    );
     mouseClick()
 })
 
@@ -27,14 +34,33 @@ const mouseClick = () => {
         // 椭球面坐标
         let pickEllipsoid = viewer.camera.pickEllipsoid(movement.position, viewer.scene.ellipsoid)
         form.cartesian3 = `x:${round(pickEllipsoid.x)}, y:${round(pickEllipsoid.y)}, z:${round(pickEllipsoid.z)}`
+        // 场景坐标
+        let pickPosition = viewer.scene.pickPosition(movement.position)
+        form.pickPosition = `x:${round(pickPosition.x)}, y:${round(pickPosition.y)}, z:${round(pickPosition.z)}`
+        // 模型经纬度
+        let modelCartographic = Cesium.Cartographic.fromCartesian(pickPosition)
+        let modelLng = Cesium.Math.toDegrees(modelCartographic.longitude).toFixed(6)
+        let modelLat = Cesium.Math.toDegrees(modelCartographic.latitude).toFixed(6)
+        form.modelCartographic = `x:${modelLng}, y:${modelLat}, z:${modelCartographic.height.toFixed(4)}`
         // 地标坐标
         let ray = viewer.camera.getPickRay(movement.position)
         var rayPosition = viewer.scene.globe.pick(ray, viewer.scene);
         form.rayPosition = `x:${round(rayPosition.x)}, y:${round(rayPosition.y)}, z:${round(rayPosition.z)}`
-        // 场景坐标
-        let pickPosition = viewer.scene.pickPosition(movement.position)
-        form.pickPosition = `x:${round(pickPosition.x)}, y:${round(pickPosition.y)}, z:${round(pickPosition.z)}`
-        
+        // 地面经纬度
+        let rayCartographic = Cesium.Cartographic.fromCartesian(rayPosition)
+        let rayLng = Cesium.Math.toDegrees(rayCartographic.longitude).toFixed(6)
+        let rayLat = Cesium.Math.toDegrees(rayCartographic.latitude).toFixed(6)
+        form.rayCartographic = `x:${rayLng}, y:${rayLat}, z:${rayCartographic.height.toFixed(4)}`
+        // 获取Cesium3DTileFeature
+        const feature = viewer.scene.pick(movement.position)
+        if(feature instanceof Cesium.Cesium3DTileFeature ) {
+            feature.color = Cesium.Color.RED
+        }
+        // 获取Cesium3DTileFeature集合
+        const featureArray = viewer.scene.drillPick(movement.position)
+        featureArray.forEach(feature => {
+            feature.color = Cesium.Color.BLUE
+        })
     }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
 }
 
@@ -56,14 +82,14 @@ const round = (number) => {
             <el-form-item label= "场景坐标" >
                 <el-input v-model="form.pickPosition" />
             </el-form-item>
+            <el-form-item label= "模型经纬度" >
+                <el-input v-model="form.modelCartographic" />
+            </el-form-item>
             <el-form-item label= "地标坐标" >
                 <el-input v-model="form.rayPosition" />
             </el-form-item>
-            <el-form-item label= "drillPick" >
-                <el-input v-model="form.pickPosition" />
-            </el-form-item>
-            <el-form-item label= "scenePick" >
-                <el-input v-model="form.pickPosition" />
+            <el-form-item label= "地面经纬度" >
+                <el-input v-model="form.rayCartographic" />
             </el-form-item>
         </el-form>
     </div>
@@ -81,7 +107,7 @@ const round = (number) => {
     top: 10px; /* 距离顶部 10px */
     left: 10px; /* 距离左侧 10px */
     z-index: 10; /* 提高层级，确保浮层显示在地图上方 */
-    width: 35vw;
+    width: 27vw;
     /* height: 30vh; */
     /* background: rgb(24, 184, 32); */
 }
